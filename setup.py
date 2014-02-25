2bff8f5f6f3f26dd9b0db33af777816d7c20d9a1
Minh Danh <minhdanh@tgm.vn>
2014-02-25 18:06:23 +0700

    Add Readme, Manifest

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='hellosign-python-sdk',
       version='0.1',
       description='An API wrapper written in Python to use with HelloSign\'s API (http://www.hellosign.com)',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      
+      keywords='funniest joke comedy flying circus',
       url='http://github.com/storborg/hellosign-python-sdk',
       author='Minh Danh',
       author_email='minhdanh@siliconstraits.vn',
       license='MIT',
       packages=['hellosign-python-sdk'],
+      install_requires=[
+          'markdown',
+      ],
       zip_safe=False)
