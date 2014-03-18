950757864bcf508d282238bea10c21ee8170609b
Minh Danh <minhdanh@tgm.vn>
2014-03-18 17:41:02 +0700

    Add example directory

---
+++ hellosign_python_sdk/example/django_site/django_site/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from hellosign import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_site.views.home', name='home'),
+    # url(r'^django_site/', include('django_site.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'hellosign.views.index')
+)
+urlpatterns += staticfiles_urlpatterns()
