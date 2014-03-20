2f6979e12bbba1366a14f5cb9c8378271b4f3055
Minh Danh <minhdanh@tgm.vn>
2014-03-20 10:09:02 +0700

    Update to version 0.1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
         return f.read()
 
 setup(name='hellosign-python-sdk',
-      version='0.1.3',
+      version='0.1.4',
       description="An API wrapper written in Python to interact with \
             HelloSign's API (http://www.hellosign.com)",
       long_description=readme(),
