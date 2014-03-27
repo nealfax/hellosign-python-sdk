0ecd5197b609753c4ed48b5957811bde77cd9f6b
Minh Danh <minhdanh@tgm.vn>
2014-03-27 16:00:33 +0700

    Fix tests

--- hellosign_python_sdk/tests/functional_tests/test_reusable_form.py
+++ hellosign_python_sdk/tests/functional_tests/test_reusable_form.py
@@ -32,29 +32,28 @@ class TestReusableForm(TestCase):
                 self.assertEquals(team.name, "Team Name")
                 create_team = True
             try:
-                new_team = self.client.add_team_member("demo_user_1@example.com")
+                new_team = self.client.add_team_member("demo@example.com")
                 print team.accounts
                 self.assertTrue(isinstance(new_team, Team))
                 team = new_team
+                self.assertTrue("demo@example.com" in [account["email_address"].encode('UTF8') for account in team.accounts])
             except Forbidden, e:
-                # self.fail(str(e))
+                self.fail(str(e))
                 # This account is already on your team
-                pass
-            self.assertTrue("demo_user_1@example.com" in [account["email_address"].encode('UTF8') for account in team.accounts])
             rf = self.client.get_reusable_form(rfl[0].reusable_form_id)
             self.assertTrue(isinstance(rf, ReusableForm))
 
             rf = self.client.add_user_to_reusable_form(
-                rfl[0].reusable_form_id, None, "demo_user_1@example.com")
+                rfl[0].reusable_form_id, None, "demo@example.com")
             self.assertTrue(isinstance(rf, ReusableForm))
 
             rf = self.client.remove_user_from_reusable_form(
-                rfl[0].reusable_form_id, None, "demo_user_1@example.com")
+                rfl[0].reusable_form_id, None, "demo@example.com")
             self.assertTrue(isinstance(rf, ReusableForm))
 
-            new_team = self.client.remove_team_member("demo_user_1@example.com")
+            new_team = self.client.remove_team_member("demo@example.com")
             self.assertTrue(isinstance(team, Team))
-            self.assertFalse("demo_user_1@example" in [account["email_address"] for account in team.accounts])
+            self.assertFalse("demo@example" in [account["email_address"].encode('UTF8') for account in team.accounts])
 
             if create_team is True:
                 result = self.client.destroy_team()
